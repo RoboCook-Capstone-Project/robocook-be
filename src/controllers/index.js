cc8ee80063acd57e7e4c07efe578750a55a0ca6a
Adelline Kania <adellinekaniasetiyawan@gmail.com>
2023-11-24 23:58:06 +0700

    add recipe controller to index

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,13 @@
 import authController from "./auth.controller.js";
 import userController from "./user.controller.js";
 import postController from "./post.controller.js";
+import recipeController from "./recipe.controller.js";
 import galleryController from "./gallery.controller.js";
 
-export { authController, userController, postController, galleryController };
+export {
+    authController,
+    userController,
+    postController,
+    recipeController,
+    galleryController,
+};
