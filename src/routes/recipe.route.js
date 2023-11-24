4e984e7be084c2aa5bba895975ac10d37aadb428
Adelline Kania <adellinekaniasetiyawan@gmail.com>
2023-11-25 00:15:36 +0700

    add fyp validation as router middleware

--- src/routes/recipe.route.js
+++ src/routes/recipe.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 
 import { recipeController } from "../controllers/index.js";
+import validate from "../middlewares/validate.js";
+import recipeValidation from "../validations/recipe.validation.js";
 
 const recipeRouter = express.Router();
 
-recipeRouter.route("/").get(recipeController.getRecipes);
+recipeRouter
+    .route("/")
+    .get(validate(recipeValidation.forYouPage), recipeController.getRecipes);
 
 export default recipeRouter;
