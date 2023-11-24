768429ae78cfbf4d913c4caa925d228770b4b648
Adelline Kania <adellinekaniasetiyawan@gmail.com>
2023-11-25 00:55:25 +0700

    add validation to routes register

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 
 import { authController } from "../controllers/index.js";
+import validate from "../middlewares/validate.js";
+import { authValidation } from "../validations/index.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", authController.register);
+authRouter
+    .route("/register")
+    .post(validate(authValidation.createUser), authController.register);
+
 authRouter.post("/login", authController.login);
 authRouter.post("/logout", authController.logout);
 authRouter.post("/refresh-tokens", authController.refreshTokens);
